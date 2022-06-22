573ccd458cc9b47f0665ed0e257c91ca3975e6ef
Marco Ballario <marco.ballario1997@gmail.com>
2022-06-22 13:14:04 +0200

    Create catalogue-service module

--- wa2-g12-ticket-catalogue-service/src/main/kotlin/it/polito/wa2/g12/ticketcatalogueservice/dto/OrderDTO.kt
+++
@@ -1,9 +0,0 @@
-package it.polito.wa2.g12.ticketcatalogueservice.dto
-
-class OrderDTO(
-    val id: Long,
-    val quantity: Int,
-    val status: String,
-    val username: String,
-    val ticket_id: Long
-)
--- wa2-g12-ticket-catalogue-service/src/main/kotlin/it/polito/wa2/g12/ticketcatalogueservice/dto/UserDetailsDTO.kt
+++
@@ -1,6 +0,0 @@
-package it.polito.wa2.g12.ticketcatalogueservice.dto
-
-data class UserDetailsDTO(
-    val username: String,
-    val roles : List<String>
-)
